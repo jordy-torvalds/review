e8ed4cbc5ce6791b073c738486243a4ccaf57937
isemang <a55306039@gmail.com>
2022-06-14 00:50:02 +0900

    feat - webclienthelper header 설정 및 content type 설정 변경

--- src/main/java/com/kis/searchaddress/api/helper/WebClientHelper.java
+++ src/main/java/com/kis/searchaddress/api/helper/WebClientHelper.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.annotation.PostConstruct;
@@ -25,7 +26,20 @@ public class WebClientHelper {
 
     @PostConstruct
     public void initWebClient() {
-        webClient = WebClient.create(ApiConfig.API_SERVER_HOST);
+        webClient = WebClient.builder().baseUrl(ApiConfig.API_SERVER_HOST)
+                .defaultHeader("Authorization","KakaoAK " + apiKey.getAdminKey())
+                .defaultHeader("Content-Type","application/json; charset=utf-8")
+                .filter(logFilter())
+                .build();
+    }
+
+    private ExchangeFilterFunction logFilter() {
+        return (clientRequest, next) -> {
+            log.info("Request URL {}", clientRequest.url());
+            log.info("Header {}",clientRequest.headers());
+            log.info("Body {}",clientRequest.body());
+            return next.exchange(clientRequest);
+        };
     }
 
     public String request(final String apiPath) {
@@ -51,12 +65,8 @@ public class WebClientHelper {
         requestUrl.append(apiPath + ".json");
         requestUrl.append("?");
         requestUrl.append(paramStr);
-
         return webClient.get()
                 .uri(requestUrl.toString())
-                .headers(h -> h.set("Authorization", "KakaoAK " + apiKey.getAdminKey()))
-                .headers(h -> h.set("Content-Type", String.valueOf(MediaType.APPLICATION_FORM_URLENCODED)))
-                .acceptCharset(CharsetUtil.UTF_8)
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
@@ -65,11 +75,8 @@ public class WebClientHelper {
     public String postRequest(final String apiPath, final Map<String, Object> params) {
         String requestUrl = apiPath + ".json";
         String paramStr = mapToHttpParams(params);
-
         return webClient.post()
                 .uri(requestUrl)
-                .headers(h -> h.set("Authorization", "KakaoAK " + apiKey.getAdminKey()))
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(paramStr, String.class)
                 .retrieve()
                 .bodyToMono(String.class)
