24b12e58c7873bd940a6ef248b371bacb618c70c
isemang <a55306039@gmail.com>
2022-06-14 09:56:24 +0900

    feat - api 결과값 view에 전달 시 DTO 이용, DTO 추가

---
+++ src/main/java/com/kis/searchaddress/dto/response/view/ApiResultResponseDTO.java
@@ -0,0 +1,16 @@
+package com.kis.searchaddress.dto.response.view;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResultResponseDTO {
+
+    String resultCd;
+    String addressResult;
+}
