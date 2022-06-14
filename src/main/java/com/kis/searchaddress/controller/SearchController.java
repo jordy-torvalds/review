739d1d85782b76b7cf11998a9c2ff43e8f5db548
isemang <a55306039@gmail.com>
2022-06-14 09:57:02 +0900

    feat - api 결과값 view에 전달 시 DTO 이용 및 json으로 결과값 전달

--- src/main/java/com/kis/searchaddress/controller/SearchController.java
+++ src/main/java/com/kis/searchaddress/controller/SearchController.java
@@ -1,10 +1,12 @@
 package com.kis.searchaddress.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kis.searchaddress.dto.response.history.HistoryResponseDTO;
 import com.kis.searchaddress.dto.request.ApiRequestDTO;
 import com.kis.searchaddress.dto.response.kakaoApi.address.AddressApiResponseDTO;
 import com.kis.searchaddress.dto.response.kakaoApi.keyword.KeywordApiResponseDTO;
+import com.kis.searchaddress.dto.response.view.ApiResultResponseDTO;
 import com.kis.searchaddress.service.ApiServiceImpl;
 import com.kis.searchaddress.service.HistoryServiceImpl;
 import lombok.extern.slf4j.Slf4j;
@@ -31,39 +33,58 @@ public class SearchController {
     @Autowired
     HistoryServiceImpl historyService;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @PostMapping("/api")
     public String addressSearch(@RequestBody ApiRequestDTO dto) {
         log.info("dto.toString(): "+dto.toString());
-        String result = "{}";
+        ApiResultResponseDTO apiResult;
+        String apiResultJson = "";
 
         try {
             AddressApiResponseDTO addressApiDTO = apiService.addressApi(dto);
             KeywordApiResponseDTO keywordApiDTO = apiService.keywordApi(dto);
 
-            String s = apiService.searchResult(addressApiDTO, keywordApiDTO);
+            String searchResult = apiService.searchResult(addressApiDTO, keywordApiDTO);
 
-            log.info(s);
-            result = "{\"result\":\""+s+"\"}";
-            log.info(result);
+            log.info("searchResult::: "+ searchResult);
 
-            HistoryResponseDTO historyDTO = new HistoryResponseDTO(dto.toString(), s);
+            apiResult = new ApiResultResponseDTO("S", searchResult);
+
+            HistoryResponseDTO historyDTO = new HistoryResponseDTO(dto.toString(), searchResult);
             log.info(historyDTO.toString());
 
             historyService.saveHistory(historyDTO);
         } catch (JsonProcessingException e) {
             log.info("[addressSearch] JsonProcessingException 발생::: "+ e.getMessage());
+
+            apiResult = new ApiResultResponseDTO("F","");
         }
 
-        return result;
+        try {
+            apiResultJson = objectMapper.writeValueAsString(apiResult);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return apiResultJson;
     }
 
     @PostMapping("/history")
     public String historySearch() {
         List<HistoryResponseDTO> allHistory = historyService.findAllHistory();
+        String historyJson = "";
 
         for (int i = 0; i < allHistory.size(); i++) {
             log.info(i + " ::: "+ allHistory.get(i).toString());
         }
-        return "{}";
+
+        try {
+            historyJson = objectMapper.writeValueAsString(allHistory);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return historyJson;
     }
 }
